00ac307d1d1275fa8af677f9006119f5173168de
Laurent Almeras <lalmeras@gmail.com>
2020-12-21 00:24:45 +0100

    installer refactor

--- jploot-core/src/main/java/jploot/core/installer/JplootInstaller.java
+++ jploot-core/src/main/java/jploot/core/installer/JplootInstaller.java
@@ -25,6 +25,9 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.pivovarit.function.ThrowingSupplier;
+import com.pivovarit.function.exception.WrappedException;
+
 import eu.mikroskeem.picomaven.DownloadResult;
 import eu.mikroskeem.picomaven.PicoMaven;
 import eu.mikroskeem.picomaven.artifact.Dependency;
@@ -39,92 +42,135 @@ public class JplootInstaller {
 	private static final URI JPLOOT_SNAPSHOTS_REPOSITORY = URI.create("http://localhost:8081/repository/jploot-snapshots/");
 	private static final URI JPLOOT_RELEASES_REPOSITORY = URI.create("http://localhost:8081/repository/jploot-releases/");
 
+	private enum Step {
+		TEMP_DIR,
+		DOWNLOAD_APPLICATION,
+		LOADING_APPLICATION_PROPERTIES,
+		DOWNLOAD_APPLICATION_DEPENDENCIES,
+		INSTALL_APPLICATION;
+	}
+
 	public void run(JplootConfig config, JplootApplication application) {
-		Path temp = null;
+		Path targetRepository = config.repository();
+		Step step = Step.TEMP_DIR;
+		List<Runnable> finallyTasks = new ArrayList<>();
 		try {
-			temp = Files.createTempDirectory(null);
-			Path myTemp = temp;
-			Path targetRepository = config.repository();
-			LOGGER.debug("Running {} in {}", config, application);
-			List<Dependency> dependencies = Arrays.<Dependency>asList(
-					new Dependency(application.groupId(), application.artifactId(), application.version(),
-							null, false /* no transitive lookup */, Collections.emptyList()));
+			Path temp = ThrowingSupplier.unchecked(() -> Files.createTempDirectory(null)).get();
+			finallyTasks.add(() -> deleteFolderRecursively(temp));
+			List<Dependency> dependencies = Arrays.asList(applicationToDependency(application));
+			step = Step.DOWNLOAD_APPLICATION;
 			List<DownloadResult> downloaded = downloadDependencies(temp, dependencies);
+			step = Step.LOADING_APPLICATION_PROPERTIES;
 			String jarUrl = String.format("jar:file://%s!%s", downloaded.get(0).getArtifactPath(), "/META-INF/jploot/jploot.properties");
-			try {
-				URL url = new URL(jarUrl);
-				try (InputStream is = url.openConnection().getInputStream(); Reader reader = new InputStreamReader(is)) {
-					Properties properties = new Properties();
-					properties.load(reader);
-					String dependenciesString = (String) properties.getOrDefault("classpathDependencies", "");
-					List<Dependency> appDependencies = Arrays.stream(dependenciesString.split(" "))
-							.map(String::strip)
-							.map(JplootInstaller::dependencySpecToDependency)
-							.collect(Collectors.toList());
-					downloaded.addAll(downloadDependencies(myTemp, appDependencies));
-				} catch (IOException e) {
-					throw new RuntimeException(String.format("Failure reading property file %s", jarUrl), e);
-				}
-			} catch (MalformedURLException e) {
-				throw new RuntimeException(String.format("URI format error for jar resource %s", jarUrl), e);
-			}
-			List<DownloadResult> failedDownloads = downloaded.stream()
-					.filter(Predicate.not(DownloadResult::isSuccess))
-					.collect(Collectors.toList());
-			if (!failedDownloads.isEmpty()) {
-				throw new RuntimeException(String.format("Dependency lookup failed on %s",
-						String.join(" ", failedDownloads.stream().map(DownloadResult::toString).collect(Collectors.toList()))));
-			}
+			Properties properties = loadProperties(jarUrl);
+			List<Dependency> appDependencies = collectDependencies(properties);
+			step = Step.DOWNLOAD_APPLICATION_DEPENDENCIES;
+			downloaded.addAll(downloadDependencies(temp, appDependencies));
+			step = Step.INSTALL_APPLICATION;
 			downloaded.stream()
-				.filter(r -> r.getArtifactPath().getFileName().toString().endsWith(".jar"))
-				.forEach(r -> {
-					Path target = targetRepository.resolve(myTemp.relativize(r.getArtifactPath()));
-					if (target.toFile().exists()) {
-						return;
-					}
-					Path parent = target.getParent();
-					if (!parent.toFile().isDirectory()) {
-						parent.toFile().mkdirs();
-					}
-					try {
-						Files.copy(r.getArtifactPath(), target);
-					} catch (IOException e) {
-						throw new RuntimeException(
-								String.format("Error copying %s to %s", r.getArtifactPath(), target), e);
-					}
-				});
-		} catch (IOException e1) {
-			throw new RuntimeException("Error creating temporary file", e1);
+				.filter(this::isJarFile)
+				.forEach(r -> installArtifact(temp, targetRepository, r));
+		} catch (RuntimeException e) {
+			Throwable cause = e;
+			if (e instanceof WrappedException) {
+				cause = e.getCause();
+			}
+			switch (step) {
+			case TEMP_DIR:
+				throw new RuntimeException("Error creating temporary file", cause);
+			case DOWNLOAD_APPLICATION:
+				throw new RuntimeException("Download failed", cause);
+			case LOADING_APPLICATION_PROPERTIES:
+				throw new RuntimeException("Properties loading failed", cause);
+			case DOWNLOAD_APPLICATION_DEPENDENCIES:
+				throw new RuntimeException("Dependency download failed", cause);
+			case INSTALL_APPLICATION:
+				throw new RuntimeException("Install failed", cause);
+			}
 		} finally {
-			if (temp != null) {
-				try {
-					Files.walkFileTree(temp, new FileVisitor<Path>() {
-						@Override
-						public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-							return FileVisitResult.CONTINUE;
-						}
-	
-						@Override
-						public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-							Files.deleteIfExists(file);
-							return FileVisitResult.CONTINUE;
-						}
-	
-						@Override
-						public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-							return FileVisitResult.CONTINUE;
-						}
-	
-						@Override
-						public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-							Files.delete(dir);
-							return FileVisitResult.CONTINUE;
-						}});
-				} catch (IOException e) {
-					throw new RuntimeException(String.format("Error deleting temp directory %s", temp), e);
-				}
+			for (Runnable finallyTask : finallyTasks) {
+				// TODO: handle exception
+				finallyTask.run();
 			}
 		}
+	}
+
+	private boolean isJarFile(DownloadResult downloadedArtifact) {
+		return downloadedArtifact.getArtifactPath().getFileName().toString().endsWith(".jar");
+	}
+
+	private void installArtifact(Path rootFolder, Path targetRepository, DownloadResult downloadedArtifact) {
+		Path target = targetRepository.resolve(rootFolder.relativize(downloadedArtifact.getArtifactPath()));
+		if (target.toFile().exists()) {
+			return;
+		}
+		Path parent = target.getParent();
+		if (!parent.toFile().isDirectory()) {
+			parent.toFile().mkdirs();
+		}
+		try {
+			Files.copy(downloadedArtifact.getArtifactPath(), target);
+		} catch (IOException e) {
+			throw new RuntimeException(
+					String.format("Error copying %s to %s", downloadedArtifact.getArtifactPath(), target), e);
+		}
+	}
+
+	private Dependency applicationToDependency(JplootApplication application) {
+		return new Dependency(application.groupId(), application.artifactId(), application.version(),
+				null, false /* no transitive lookup */, Collections.emptyList());
+	}
+
+	private void deleteFolderRecursively(Path folder) {
+		try {
+			Files.walkFileTree(folder, new FileVisitor<Path>() {
+				@Override
+				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.deleteIfExists(file);
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}});
+		} catch (IOException e) {
+			throw new RuntimeException(String.format("Error deleting temp directory %s", folder), e);
+		}
+	}
+
+	private Properties loadProperties(String jarUrl) {
+		try {
+			URL url = new URL(jarUrl);
+			try (InputStream is = url.openConnection().getInputStream(); Reader reader = new InputStreamReader(is)) {
+				Properties properties = new Properties();
+				properties.load(reader);
+				return properties;
+			} catch (IOException e) {
+				throw new RuntimeException(String.format("Failure reading property file %s", jarUrl), e);
+			}
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(String.format("URI format error for jar resource %s", jarUrl), e);
+		}
+	}
+
+	private List<Dependency> collectDependencies(Properties properties) {
+		String dependenciesString = (String) properties.getOrDefault("classpathDependencies", "");
+		return Arrays.stream(dependenciesString.split(" "))
+				.map(String::strip)
+				.map(JplootInstaller::dependencySpecToDependency)
+				.collect(Collectors.toList());
 	}
 
 	private static Dependency dependencySpecToDependency(String dependencySpec) {
@@ -151,13 +197,23 @@ public class JplootInstaller {
 				.withDependencies(dependencies);
 		List<DownloadResult> downloaded = new ArrayList<>();
 		try (PicoMaven picoMaven = picoMavenBuilder.build()) {
-			picoMaven.downloadAllArtifacts().values().stream().map(JplootInstaller::getFuture)
+			picoMaven.downloadAllArtifacts().values().stream()
+				.map(JplootInstaller::getDownloadResult)
 				.forEach(downloaded::add);
+		}
+		List<DownloadResult> failedDownloads = downloaded.stream()
+				.filter(Predicate.not(DownloadResult::isSuccess))
+				.collect(Collectors.toList());
+		if (!failedDownloads.isEmpty()) {
+			List<String> failed = failedDownloads.stream().map(DownloadResult::toString).collect(Collectors.toList());
+			throw new RuntimeException(
+					String.format("Dependency lookup failed for %s",
+					String.join(" ", failed)));
 		}
 		return downloaded;
 	}
 
-	private static DownloadResult getFuture(Future<DownloadResult> future) {
+	private static DownloadResult getDownloadResult(Future<DownloadResult> future) {
 		while (true) {
 			try {
 				return future.get();
